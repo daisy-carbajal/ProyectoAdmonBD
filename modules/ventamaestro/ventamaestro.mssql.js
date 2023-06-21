08382cd85f25901a1c42c4f775175cf69606b10c
daisy-carbajal <dcarbajal@unitec.edu>
2023-06-20 22:19:16 -0600

    fix syntx

--- modules/ventamaestro/ventamaestro.mssql.js
+++ modules/ventamaestro/ventamaestro.mssql.js
@@ -19,10 +19,9 @@ class VentaMaestroMSSql {
   async addVentaMaestro(venta) {
     const conn = await mssqlcon.getConnection();
     const res = await conn.request()
-    .input("VENTA_ID", venta.VENTA_ID)
     .input("VENTA_CLIENTE_ID", venta.VENTA_CLIENTE_ID)
     .input("VENTA_FECHA", venta.VENTA_FECHA)
-    .input("VENTA_TOTAL", VENTA_TOTAL)
+    .input("VENTA_TOTAL", venta.VENTA_TOTAL)
     .execute("SP_CREAR_VENTAS");
     return res;
  }
@@ -42,7 +41,7 @@ class VentaMaestroMSSql {
     const conn = await mssqlcon.getConnection();
     const res = await conn.request()
     .input("VENTA_ID", id)
-    .execute("SP_ELIMINAR_VENTASM");
+    .execute("SP_ELIMINAR_VENTAS");
     return res;
   }
 
